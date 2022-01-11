11c3c5a086a83c6f1a9a332ac94ceda533f7e42b
Dan-Evan <65192520+Dan-Evan@users.noreply.github.com>
2022-01-11 14:00:27 -0500

    First C++ Game Workspace

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+/*
+* Author: Daniel Evans
+* Date: January 11,2022
+* Description: Prints my information
+*/
+#include <cstdio>
+
+int main()
+{
+    printf("Hello world, this is Daniel.\n");
+    printf("I live in Cessnock, Hanover.\n");
+    printf("I love to watch anime and play piano and games.\n");
+}
